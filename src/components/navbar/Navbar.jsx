99c1f23f071bd6137cff66454ada68dc57c7f010
Shaarang Tanpure <shaarang.1910@gmail.com>
2021-06-21 23:50:59 +1000

    add axios dependencies and create homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
+import './Navbar.css';
+
 const NavComponent = (props) => {
   return (
     <Navbar bg={props.bgColor} expand="lg">
       <Navbar.Brand href="#home">{props.header}</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
           <NavDropdown title={props.user} id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
-    </Navbar>
+  </Navbar>
   );
 };
 
--- src/containers/pages/homepage/Homepage.jsx
+++ src/containers/pages/homepage/Homepage.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
+import axios from "axios";
 
-import Hoc from '../../../components/hoc/Hoc';
+import { Row, Col, Button } from 'react-bootstrap';
 
-class Homepage extends Component{
-    constructor(){
-        super();
-        this.state={
+import Hoc from "../../../components/hoc/Hoc";
+import NavComponent from "../../../components/navbar/Navbar";
 
-        }
-    }
+class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    componentDidMount(){
-        // load json file
-    }
+  componentDidMount() {
+    // load json file
+    axios
+      .get("../data/groceries_list.json")
+      .then((response) => {
+        const { data } = response;
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    render(){
-        return(
-            <Hoc>
-                {/*
+  render() {
+    return (
+      <Hoc>
+        {/*
                     Render list of grocery items as a card. 
                     Card and items completed and to complete
                     May be organize by date
                     
                 */}
-            </Hoc>
-        )
-    }
+        <NavComponent bgColor='dark' header='Shopping list' user='Shaarang'/>
+        <Row>
+            <Col xs={12} md={3}>
+                <Button variant='success' size='lg' block>{ '+' }</Button>
+            </Col>
+        </Row>
+      </Hoc>
+    );
+  }
 }
-
 
 export default Homepage;
